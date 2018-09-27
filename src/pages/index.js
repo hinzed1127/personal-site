f4fb271a5acf3582f0fc88dc37e2f653a563a8e4
Dan Hinze <hinzed1127@gmail.com>
2018-09-27 01:32:45 -0500

    Open all links in new tabs.

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,12 +11,12 @@ export default () => (
       <p>
         I'm Dan Hinze, a musician and web
         developer newly relocated to Minneapolis. Before that, I was in Chicago, working at{" "}
-        <a href="https://sproutsocial.com/">Sprout Social</a>, and playing sax/writing music with{" "}
-        <a href="https://www.fayray.band/">Fay Ray</a>.
+        <a href="https://sproutsocial.com/" target="_blank" rel="noopener noreferrer">Sprout Social</a>, and playing sax/writing music with{" "}
+        <a href="https://www.fayray.band/" target="_blank" rel="noopener noreferrer">Fay Ray</a>.
       </p>
       <p>
           I'm {" "}
-          <a href="https://github.com/hinzed1127/personal-site">actively building out this site</a>, but{" "}
+          <a href="https://github.com/hinzed1127/personal-site" target="_blank" rel="noopener noreferrer">actively building out this site</a>, but{" "}
           in the meantime, feel free to <a href="mailto:dan.hinze.92@gmail.com">drop me a line</a>.
       </p>
     </div>
