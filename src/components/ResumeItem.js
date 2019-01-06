2bb9746e569d11f5d67feb3584fccbf38c4e7dbb
Dan Hinze <hinzed1127@gmail.com>
2019-01-06 14:21:22 -0600

    fixed resumeItem stuff

--- src/components/ResumeItem.js
+++ src/components/ResumeItem.js
@@ -11,16 +11,32 @@ const ItemHeader = styled.div`
   justify-content: space-between;
 `;
 
+const Place = styled.h3`
+  @media (max-width: 630px) {
+      max-width: 200px;
+      font-size: 18px;
+  }
+`;
+
+const Duration = styled.span`
+  @media (max-width: 630px) {
+      font-size: 14px;
+  }
+`;
+
 const Description = styled.div`
   margin-top: 10px;
+  @media (max-width: 630px) {
+      font-size: 14px;
+  }
 `;
 
 export default function ({ place, duration, children }) {
   return (
     <ResumeItem>
       <ItemHeader>
-        <h3>{place}</h3>
-        <span>{duration}</span>
+        <Place>{place}</Place>
+        <Duration>{duration}</Duration>
       </ItemHeader>
       <Description>{children}</Description>
     </ResumeItem>
