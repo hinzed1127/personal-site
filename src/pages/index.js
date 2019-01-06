dbc40133625db7165bd4081212fd1118ee24116d
Dan Hinze <hinzed1127@gmail.com>
2019-01-06 15:00:28 -0600

    More renaming issues

--- src/pages/index.js
+++ src/pages/index.js
@@ -31,29 +31,6 @@ const ResumeLink = styled(Link)`
   color: black;
 `;
 
-<<<<<<< HEAD
-export default () => (
-  <BaseContainer>
-    <Content>
-      <Nav />
-      <MainContainer>
-        <div className="svg-container">
-          <RaisingHand />
-        </div>
-        <div className="greeting">
-          <h2>Hi!</h2>
-          <p>
-            I'm Dan Hinze, a musician and web developer recently relocated to Minneapolis and currently at <ExternalLink href="https://bestbuy.com/">Best Buy</ExternalLink>.
-            Before that, I was in Chicago, working at <ExternalLink href="https://sproutsocial.com/">Sprout Social</ExternalLink> and playing sax/writing music with <ExternalLink href="https://www.fayray.band/">Fay Ray</ExternalLink>.
-            I'm always open to new opportunities and colloborations. Feel free to <ExternalLink href="mailto:dan.hinze.92@gmail.com">reach out.</ExternalLink>
-          </p>
-        </div>
-      </MainContainer>
-      <Footer />
-    </Content>
-  </BaseContainer>
-);
-=======
 const FullScreenCanvas = styled(DrawCanvas)`
   z-index: 1;
 `;
@@ -69,7 +46,7 @@ export default class HomePage extends React.Component {
     this.state = {
       color: 'hsl(180, 100%, 50%)',
       value: 180,
-      isDrawing: false,
+      isDrawing: false
     };
   }
 
@@ -103,7 +80,9 @@ export default class HomePage extends React.Component {
           />{' '}
         </Nav>
         <Content>
-          {this.state.isDrawing && <FullScreenCanvas color={this.state.color} />}
+          {this.state.isDrawing && (
+            <FullScreenCanvas color={this.state.color} />
+          )}
           <MainContainer>
             <div className="svg-container">
               <RaisingHand />
@@ -111,13 +90,18 @@ export default class HomePage extends React.Component {
             <div className="greeting">
               <h2>Hi!</h2>
               <p>
-                I'm Dan Hinze, a musician and web developer freshly relocated to Minneapolis. Before
-                that, I was in Chicago, working at{' '}
-                <ExternalLink href="https://sproutsocial.com/">Sprout Social</ExternalLink> and
-                playing sax/writing music with{' '}
-                <ExternalLink href="https://www.fayray.band/">Fay Ray</ExternalLink>. I'm currently
-                looking for my next gig. Check out my <ResumeLink to="/resume/">resume</ResumeLink>{' '}
-                to see if I can help you out.
+                I'm Dan Hinze, a musician and web developer freshly relocated to
+                Minneapolis. Before that, I was in Chicago, working at{' '}
+                <ExternalLink href="https://sproutsocial.com/">
+                  Sprout Social
+                </ExternalLink>{' '}
+                and playing sax/writing music with{' '}
+                <ExternalLink href="https://www.fayray.band/">
+                  Fay Ray
+                </ExternalLink>
+                . I'm currently looking for my next gig. Check out my{' '}
+                <ResumeLink to="/resume/">resume</ResumeLink> to see if I can
+                help you out.
               </p>
             </div>
           </MainContainer>
@@ -127,4 +111,3 @@ export default class HomePage extends React.Component {
     );
   }
 }
->>>>>>> WIP building snapchat lookalike
