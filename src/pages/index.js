95e97d3a9a78c3fa16f57f495d0ad6adb252c30e
Dan Hinze <a6002871@bestbuy.com>
2019-01-12 14:28:40 -0600

    WIP fix layout issues

--- src/pages/index.js
+++ src/pages/index.js
@@ -36,7 +36,7 @@ const Greeting = styled.div`
   align-items: center;
 `;
 
-export default () => (
+export const test = () => (
   <BaseContainer>
     <Content>
       <Nav />
@@ -44,19 +44,52 @@ export default () => (
         {/* <div className="svg-container"> */}
           {/* <RaisingHand /> */}
         {/* </div> */}
-        <Greeting>
-          <Doodle />
-          <h2>Hi!</h2>
-        </Greeting>
         <div>
-          <p>
-            I'm Dan Hinze, a musician and web developer recently relocated to Minneapolis and currently at <ExternalLink href="https://bestbuy.com/">Best Buy</ExternalLink>.
-            Before that, I was in Chicago, working at <ExternalLink href="https://sproutsocial.com/">Sprout Social</ExternalLink> and playing sax/writing music with <ExternalLink href="https://www.fayray.band/">Fay Ray</ExternalLink>.
-            I'm always open to new opportunities and colloborations. Feel free to <ExternalLink href="mailto:dan.hinze.92@gmail.com">reach out.</ExternalLink>
-          </p>
+          <Greeting>
+            <Doodle />
+            <h2>Hi!</h2>
+          </Greeting>
+          <div>
+            <p>
+              I'm Dan Hinze, a musician and web developer recently relocated to Minneapolis and currently at <ExternalLink href="https://bestbuy.com/">Best Buy</ExternalLink>.
+              Before that, I was in Chicago, working at <ExternalLink href="https://sproutsocial.com/">Sprout Social</ExternalLink> and playing sax/writing music with <ExternalLink href="https://www.fayray.band/">Fay Ray</ExternalLink>.
+              I'm always open to new opportunities and colloborations. Feel free to <ExternalLink href="mailto:dan.hinze.92@gmail.com">reach out.</ExternalLink>
+            </p>
+          </div>
         </div>
       </MainContainer>
       <Footer />
     </Content>
   </BaseContainer>
+);
+
+const NewMain = styled.main`
+  flex: 1 1 auto;
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  min-height: 100vh;
+`;
+
+export default () => (
+  <Container>
+    <Nav />
+    <NewMain>
+      <Greeting>
+        <Doodle />
+        <h2>Hi!</h2>
+      </Greeting>
+      <div>
+        <p>
+          I'm Dan Hinze, a musician and web developer recently relocated to Minneapolis and currently at <ExternalLink href="https://bestbuy.com/">Best Buy</ExternalLink>.
+          Before that, I was in Chicago, working at <ExternalLink href="https://sproutsocial.com/">Sprout Social</ExternalLink> and playing sax/writing music with <ExternalLink href="https://www.fayray.band/">Fay Ray</ExternalLink>.
+          I'm always open to new opportunities and collaborations. Feel free to <ExternalLink href="mailto:dan.hinze.92@gmail.com">reach out.</ExternalLink>
+        </p>
+      </div>
+    </NewMain>
+    <Footer />
+  </Container>
 );
