3f1745f0bb6ae8945c877939403a5549ee1f8ef5
Dan Hinze <a6002871@bestbuy.com>
2019-01-22 13:53:19 -0600

    Add color variables
    - Adds the 8 colors from the album cover
    - Creates utils/theme.js file, eventually use ThemeProvider

---
+++ src/utils/theme.js
@@ -0,0 +1,10 @@
+const colors = {
+  red: '#B4312F',
+  teal: '#3D889F',
+  yellow: '#D8B948',
+  green: '#367643',
+  pink: '#BF3C66',
+  darkBlue: '#17194D',
+  beige: '#E6D9C5',
+  gray: '#8E8682',
+};
