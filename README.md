0534d7d386ffaf6b9f328331943cb88f6595c902
Dan Hinze <a6002871@bestbuy.com>
2019-01-23 13:49:09 -0600

    Tweak colors to better the contrast ratio     - Switch to new color palette on pages to view results

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 This is my personal website, powered by [Gatsby](https://www.gatsbyjs.org/) and [Netlify](https://www.netlify.com).
+
+Reference this for referring to css variables while developing (make a util function to help grab them)
+https://www.ericponto.com/blog/2014/09/17/share-css-variables-with-javascript/
--- src/utils/typography.js
+++ src/utils/typography.js
@@ -3,9 +3,13 @@ import Typography from 'typography';
 const typography = new Typography({
   baseFontSize: '18px',
   basLineHeight: 1.666,
-  headerFontFamily: ['Oswald', 'Helvetica Neue', 'sans-serif'],
+  headerFontFamily: ['Roboto', 'Helvetica Neue', 'sans-serif'],
   bodyFontFamily: ['Lato', 'serif'],
   googleFonts: [
+    {
+      name: 'Roboto',
+      styles: ['400', '500']
+    },
     {
       name: 'Oswald',
       styles: ['400'],
